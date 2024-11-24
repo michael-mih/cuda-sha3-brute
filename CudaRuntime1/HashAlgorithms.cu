e8af77851acf4e71b92116e84e57559f316e5b90
Michael Mihaley <71835811+michael-mih@users.noreply.github.com>
2024-11-24 00:30:11 -0500

    Add project files.

---
+++ CudaRuntime1/HashAlgorithms.cu
@@ -0,0 +1,9 @@
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include <stdio.h>
+#include <openssl/sha.h>
+#include <string.h>
+
+int main() {
+	unsigned char inbuff[] = "test";
+}
