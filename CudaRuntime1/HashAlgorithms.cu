4432798e48b447ce8e1d54d55e75020c76e8c59b
Michael Mihaley <71835811+michael-mih@users.noreply.github.com>
2024-11-28 17:12:14 -0500

    starting cuda logic and some refactoring

--- CudaRuntime1/HashAlgorithms.cu
+++
@@ -1,9 +0,0 @@
-#include "cuda_runtime.h"
-#include "device_launch_parameters.h"
-#include <stdio.h>
-#include <openssl/sha.h>
-#include <string.h>
-
-int main() {
-	unsigned char inbuff[] = "test";
-}
