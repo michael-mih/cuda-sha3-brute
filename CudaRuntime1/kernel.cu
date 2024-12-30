66377273065cc0ce3462288170875f3779af34e7
Michael Mihaley <71835811+michael-mih@users.noreply.github.com>
2024-12-29 19:30:16 -0500

    parallel!

--- CudaRuntime1/kernel.cu
+++ CudaRuntime1/kernel.cu
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <fstream>
 
-#include "cuda_runtime.h"
+#include <cuda_runtime.h>
+#include "cuda.h"
 #include "device_launch_parameters.h"
 #include <stdio.h>
 #include <openssl/sha.h>
@@ -23,44 +24,21 @@ std::string hex_encode(std::string inpt, KeccakBase& k) {
 	return b.str();
 }
 
-int main(int argc, char *argv[]) {
-	int numThreads;
-	if (argc == 2) {
-		numThreads = atoi(argv[1]);
+__global__ void
+bruteSearch(std::string* hash, std::string** wordlist, size_t n) {
+	int index = threadIdx.x + blockIdx.x * blockDim.x; //blockDim.x threads per block
+	if (index < n) {
+		printf("index %d", index);
 	}
-	else {
-		numThreads = 2;
-	}
-	std::string raw("test");
-	std::cout << hex_encode(raw, Sha3(256));
-
-	std::string curLine;
-	std::ifstream stream("file.txt");
-	int numLines = 0;
-	while (std::getline(stream, curLine)) {
-		numLines++;
-	}
-	std::string* wordlistChunks = new std::string[numThreads];
-	std::ifstream readStream("file.txt");
 	
-	int boundary = numLines / numThreads;
-	int i = 0; int j = 0;
-	while (std::getline(readStream, curLine)) {
-		if (i < boundary) {
-			i = 0;
-			j++;
-		}
-		wordlistChunks[j] = curLine;
-		i++;
-	}
-
 }
 
 
-cudaError_t loadParallelHashes(std::string desiredHash, std::string* wordlistChunks) //todo array decay?
+cudaError_t loadParallelHashes(std::string desiredHash, std::string* wordlist, size_t wordlistLength) //todo array decay?
 {
+#define THREADS_PER_BLOCK 512;
 	std::string* dev_desiredHash = 0;
-
+	std::string** dev_wordlist = 0;
 
 	cudaError_t cudaStatus;
 	cudaStatus = cudaSetDevice(0);
@@ -69,34 +47,88 @@ cudaError_t loadParallelHashes(std::string desiredHash, std::string* wordlistChu
 		goto Error;
 	}
 	
-	//malloc for desired hash
+	//malloc and cpy for desired hash
 	cudaStatus = cudaMalloc((void **)&dev_desiredHash, desiredHash.size());
 	if (cudaStatus != cudaSuccess) {
 		fprintf(stderr, "cudaMalloc failed!");
 		goto Error;
 	}
-	
-	std::string** devPtrArr = new std::string * [wordlistChunks->size()]; //todo, pointer logic?
-	//malloc for chunk of wordlist
-	for (int i = 0; i < wordlistChunks->size(); i++) {
-		devPtrArr[i] = new std::string();
-		//todo track devptrs and divide file input 
-		cudaStatus = cudaMalloc((void **)&devPtrArr[i], desiredHash.size() / wordlistChunks->size());
-	}
-
 	cudaStatus = cudaMemcpy(dev_desiredHash, &desiredHash, desiredHash.size(), cudaMemcpyHostToDevice);
 	if (cudaStatus != cudaSuccess) {
 		fprintf(stderr, "cudaMemcpy failed!");
 		goto Error;
 	}
 
-	for (int i = 0; i < numThreads; i++) {
-		cudaStatus = cudaMemcpy(devPtrArr[i], )
+	//malloc and cpy for wordlist
+	cudaStatus = cudaMalloc((void ***)&dev_wordlist, wordlistLength);
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaMalloc failed!");
+		goto Error;
+	}
+	cudaStatus = cudaMemcpy(dev_wordlist, &wordlist, wordlistLength, cudaMemcpyHostToDevice);
+
+	int numBlocks = wordlistLength / THREADS_PER_BLOCK; 
+
+	//bruteSearch<<<(wordlistLength/THREADS_PER_BLOCK), THREADS_PER_BLOCK>>>(dev_desiredHash, dev_wordlist);
+	bruteSearch <<<1, 512 >> > (dev_desiredHash, dev_wordlist, wordlistLength);
+
+	cudaStatus = cudaGetLastError();
+	if (cudaStatus != cudaSuccess)
+	{
+		fprintf(stderr, "Failed to launch. (error code %s)!\n", cudaGetErrorString(cudaStatus));
+		exit(EXIT_FAILURE);
 	}
 
 
 Error:
 	return cudaStatus;
 }
+
+int main(int argc, char* argv[]) {
+	int numThreads;
+	if (argc == 2) {
+		numThreads = atoi(argv[1]);
+	}
+	else {
+		numThreads = 2;
+	}
+	/*std::string raw("test");
+	std::cout << hex_encode(raw, Sha3(256)); */
+
+	std::string curLine;
+	std::ifstream stream("file.txt");
+	int numLines = 0;
+	while (std::getline(stream, curLine)) {
+		numLines++;
+	}
+	std::string* wordlist = new std::string[numLines];
+	std::ifstream readStream("file.txt");
+	int i = 0;
+	while (std::getline(readStream, curLine)) {
+		wordlist[i] = curLine;
+		std::cout << wordlist[i] << '\n';
+		i++;
+	}
+
+	/*int boundary = numLines / numThreads;
+	int i = 0; int j = 0;
+
+	while (std::getline(readStream, curLine)) {
+		if (i > boundary) {
+			i = 0;
+			j++;
+		}
+		wordlistChunks[j] += curLine;
+		i++;
+	} */
+
+
+	std::cout << "starting parallelization" << '\n';
+	
+	loadParallelHashes("test", wordlist, numLines);
+
+	
+}
+
 
 	
